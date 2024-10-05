e8cb85f7a96e8fc27facac687fa5fe46ea963eb6
Toluwaloju0 <teecoded1999@gmail.com>
2024-10-05 21:27:21 +0100

    javascript

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,5 +1,15 @@
-import [ uploadPhoto , createUser ] from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function () {
-  
+  const newObj = {};
+
+  await uploadPhoto().then((obj) => {
+    newObj.photo = obj;
+  });
+
+  await createUser().then((obj) => {
+    newObj.user = obj;
+  });
+
+  return newObj;
 }
