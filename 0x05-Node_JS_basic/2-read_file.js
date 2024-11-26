04c430206e9a4ce4548d754be4f2c94c011d6129
Toluwaloju0 <teecoded1999@gmail.com>
2024-11-26 19:42:43 +0100

    node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,23 +5,23 @@ function countStudents(path) {
     const data = fs.readFileSync(path, 'utf-8');
     const dataList = data.split('\n');
     let total = 0;
-    const CE = [];
+    const CS = [];
     const SWE = [];
     for (let index = 1; index < dataList.length - 1; index += 1) {
       const student = dataList[index].split(',');
       if (student[3] === 'CS') {
         total += 1;
-        CE.push(student[0]);
+        CS.push(student[0]);
       } else if (student[3] === 'SWE') {
         total += 1;
         SWE.push(student[0]);
       }
     }
     process.stdout.write(`Number of students: ${total}\n`);
-    process.stdout.write(`Number of students in CE: ${CE.length}. \
-List ${CE.toString().replaceAll(',', ', ')}\n`);
+    process.stdout.write(`Number of students in CS: ${CS.length}. \
+List: ${CS.toString().replaceAll(',', ', ')}\n`);
     process.stdout.write(`Number of students in SWE: ${SWE.length}. \
-List ${SWE.toString().replaceAll(',', ', ')}\n`);
+List: ${SWE.toString().replaceAll(',', ', ')}\n`);
   } catch (err) {
     console.log(err);
     throw new Error('Cannot load the database');
