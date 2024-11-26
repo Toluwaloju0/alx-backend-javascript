ff65e4c268009c219ceb2e83435f07b0d6f72102
Toluwaloju0 <teecoded1999@gmail.com>
2024-11-26 14:38:51 +0100

    reafd file sync

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,9 +1,11 @@
 const fs = require('node:fs');
 
 function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf-8');
-    const dataList = data.split('\n');
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    const dataList = data.toString().split('\n');
     let total = 0;
     const CE = [];
     const SWE = [];
@@ -20,10 +22,33 @@ function countStudents(path) {
     process.stdout.write(`Number of students: ${total}\n`);
     process.stdout.write(`Number of students in CE: ${CE.length}. List ${CE.toString().replaceAll(',', ', ')}\n`);
     process.stdout.write(`Number of students in SWE: ${SWE.length}. List ${SWE.toString().replaceAll(',', ', ')}\n`);
-  } catch (err) {
-    console.log(err);
-    throw new Error('Cannot load the database');
-  }
+  });
 }
+
+// function countStudents(path) {
+//   try {
+//     const data = fs.readFileSync(path, 'utf-8');
+//     const dataList = data.split('\n');
+//     let total = 0;
+//     const CE = [];
+//     const SWE = [];
+//     for (let index = 1; index < dataList.length - 1; index += 1) {
+//       const student = dataList[index].split(',');
+//       if (student[3] === 'CS') {
+//         total += 1;
+//         CE.push(student[0]);
+//       } else if (student[3] === 'SWE') {
+//         total += 1;
+//         SWE.push(student[0]);
+//       }
+//     }
+//     process.stdout.write(`Number of students: ${total}\n`);
+//     process.stdout.write(`Number of students in CE: ${CE.length}. List ${CE.toString().replaceAll(',', ', ')}\n`);
+//     process.stdout.write(`Number of students in SWE: ${SWE.length}. List ${SWE.toString().replaceAll(',', ', ')}\n`);
+//   } catch (err) {
+//     console.log(err);
+//     throw new Error('Cannot load the database');
+//   }
+// }
 
 module.exports = countStudents;
