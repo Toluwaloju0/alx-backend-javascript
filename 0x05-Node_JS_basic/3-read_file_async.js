3566f58bee47b17206d50109492f8637f0176ff6
Toluwaloju0 <teecoded1999@gmail.com>
2024-11-27 07:47:09 +0100

    read file sync

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,35 +1,29 @@
 const fs = require('fs');
 
-async function countStudents(path) {
-  promise = new Promise((resolve, reject) => {
-    try {
-      fs.readFile(path, (err, data) => {
-        const dataList = data.toString().split('\n');
-        let total = 0;
-        const CS = [];
-        const SWE = [];
-        for (let index = 1; index < dataList.length - 1; index += 1) {
-          const student = dataList[index].split(',');
-          if (student[3] === 'CS') {
-            total += 1;
-            CS.push(student[0]);
-          } else if (student[3] === 'SWE') {
-            total += 1;
-            SWE.push(student[0]);
-          }
+function countStudents(path) {
+  return fs.promises.readFile(path)
+    .then((data) => {
+      const dataList = data.toString().split('\n');
+      let total = 0;
+      const CS = [];
+      const SWE = [];
+      for (let index = 1; index < dataList.length - 1; index += 1) {
+        const student = dataList[index].split(',');
+        if (student[3] === 'CS') {
+          total += 1;
+          CS.push(student[0]);
+        } else if (student[3] === 'SWE') {
+          total += 1;
+          SWE.push(student[0]);
         }
-        console.log(`Number of students: ${total}`);
-        console.log(`Number of students in CS: ${CS.length}. \
-List: ${CS.toString().replaceAll(',', ', ')}`);
-        console.log(`Number of students in SWE: ${SWE.length}. \
-List: ${SWE.toString().replaceAll(',', ', ')}`);
-      });
-    } catch (err) {
+      }
+      console.log(`Number of students: ${total}`);
+      console.log(`Number of students in CS: ${CS.length}. List: ${CS.toString().replaceAll(',', ', ')}`);
+      console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.toString().replaceAll(',', ', ')}`);
+    })
+    .catch(() => {
       throw new Error('Cannot load the database');
-    }
-  });
-  return promise;
+    });
 }
-  
 
 module.exports = countStudents;
