bf18238cf17b7cf2793c01d591d4362fb536a636
Toluwaloju0 <teecoded1999@gmail.com>
2024-11-27 07:08:29 +0100

    reasFile

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,31 +1,35 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 
 async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, { encoding: 'utf-8' });
-    const dataList = data.split('\n');
-    let total = 0;
-    const CS = [];
-    const SWE = [];
-    for (let index = 1; index < dataList.length - 1; index += 1) {
-      const student = dataList[index].split(',');
-      if (student[3] === 'CS') {
-        total += 1;
-        CS.push(student[0]);
-      } else if (student[3] === 'SWE') {
-        total += 1;
-        SWE.push(student[0]);
-      }
-    }
-    console.log(`Number of students: ${total}`);
-    console.log(`Number of students in CS: ${CS.length}. \
+  promise = new Promise((resolve, reject) => {
+    try {
+      fs.readFile(path, (err, data) => {
+        const dataList = data.toString().split('\n');
+        let total = 0;
+        const CS = [];
+        const SWE = [];
+        for (let index = 1; index < dataList.length - 1; index += 1) {
+          const student = dataList[index].split(',');
+          if (student[3] === 'CS') {
+            total += 1;
+            CS.push(student[0]);
+          } else if (student[3] === 'SWE') {
+            total += 1;
+            SWE.push(student[0]);
+          }
+        }
+        console.log(`Number of students: ${total}`);
+        console.log(`Number of students in CS: ${CS.length}. \
 List: ${CS.toString().replaceAll(',', ', ')}`);
-    console.log(`Number of students in SWE: ${SWE.length}. \
+        console.log(`Number of students in SWE: ${SWE.length}. \
 List: ${SWE.toString().replaceAll(',', ', ')}`);
-    return Promise.resolve(data);
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+      });
+    } catch (err) {
+      throw new Error('Cannot load the database');
+    }
+  });
+  return promise;
 }
+  
 
 module.exports = countStudents;
