0065965f505f0b3b649acb664daebe9dff59dcaf
Toluwaloju0 <teecoded1999@gmail.com>
2024-12-01 21:15:34 +0100

    test

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,26 @@
-const { expect } = require('chai');
+const { expect } = import('chai');
 const sinon = require('sinon');
-const app = require('./api');
+// const app = require('./api');
 const request = require('request');
 
-describe('Index page', function() {
-  it('should check GET /', function() {
+
+describe('Index page tests with pre-running server', function() {
+  it('should check GET /', function(done) {
     request('http://localhost:7865/', (err, res, body) => {
+      if (err) return done(err);
       expect(res.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome to the payment system')
+      expect(body).to.equal('Welcome to the payment system');
+      done();
     });
   });
 });
+
+// describe('Index page', function() {
+//   it('should check GET /', function(done) {
+//     request('http://localhost:7865/', (err, res, body) => {
+//       expect(res.statusCode).to.equal(200);
+//       expect(body).to.equal('Welcome to the payment system');
+//       done()
+//     });
+//   });
+// });
