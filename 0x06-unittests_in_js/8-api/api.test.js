2d1153fa1d31baf604191ecb81a403064054f03c
Toluwaloju0 <teecoded1999@gmail.com>
2024-12-01 20:45:44 +0100

    tests

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,10 +1,10 @@
-const { expect } = request('chai');
+const { expect } = require('chai');
 const sinon = require('sinon');
 const app = require('./api');
 const request = require('request');
 
-describe('Index Suite', function() {
-  it('should check the index page', function() {
+describe('Index page', function() {
+  it('should check GET /', function() {
     request('http://localhost:7865/', (err, res, body) => {
       expect(res.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system')
