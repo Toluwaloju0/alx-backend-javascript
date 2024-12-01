76692725b20ed42545ab5034e0b5cb1afd910eb0
Toluwaloju0 <teecoded1999@gmail.com>
2024-12-01 15:04:18 +0100

    test

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -3,11 +3,13 @@ const getPaymentTokenFromAPI = require('./6-payment_token')
 
 const expect = chai.expect;
 
-describe.only('getPaymentTokenFromAPI', function() {
+describe('getPaymentTokenFromAPI', function() {
   it('should get the promise if the bool value is true', function(done) {
     getPaymentTokenFromAPI(true).then((data) => {
-      expect(data).to.equal({data: 'Successful response from the API'});
+      expect(data).to.deep.equal({data: 'Successful response from the API'});
+      done();
+    }).catch((err) => {
+      done(err);
     });
-    done();
   });
 });
