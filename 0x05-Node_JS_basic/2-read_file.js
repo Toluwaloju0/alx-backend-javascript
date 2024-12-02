6b00bb718a1977164e49ef024226bfd8975cd236
Toluwaloju0 <teecoded1999@gmail.com>
2024-12-02 21:35:23 +0100

    node files

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,28 +1,29 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf-8');
-    const dataList = data.split('\n');
-    let total = 0;
+  // 
+  
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      throw new Error("Cannot load the database");
+    }
     const CS = [];
     const SWE = [];
-    for (let index = 1; index < dataList.length; index += 1) {
-      const student = dataList[index].split(',');
-      if (student[3] === 'CS') {
-        total += 1;
-        CS.push(student[0]);
-      } else if (student[3] === 'SWE') {
-        total += 1;
-        SWE.push(student[0]);
+
+    data.toString().split('\n').forEach((student) => {
+      const studentData = student.split(',');
+
+      if (studentData[3] === 'SWE') {
+        SWE.push(studentData[0]);
+      } else if (studentData[3] === 'CS') {
+        CS.push(studentData[0]);
       }
-    }
-    console.log(`Number of students: ${total}`);
+    });
+
+    console.log(`Number of students: ${CS.length + SWE.length}`);
     console.log(`Number of students in CS: ${CS.length}. List: ${CS.toString().replaceAll(',', ', ')}`);
     console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.toString().replaceAll(',', ', ')}`);
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+  });
 }
 
 module.exports = countStudents;
