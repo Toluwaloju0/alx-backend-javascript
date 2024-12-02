418fe83f9bbdf1b870447609cf409e46475999fd
Toluwaloju0 <teecoded1999@gmail.com>
2024-12-02 22:06:18 +0100

    node files

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  // 
-  
+  //
+
   fs.readFile(path, 'utf-8', (err, data) => {
     if (err) {
-      throw new Error("Cannot load the database");
+      throw new Error('Cannot load the database');
     }
     const CS = [];
     const SWE = [];
